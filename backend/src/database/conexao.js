8f9b2b56a236d10a54a20f5049b0537f1cf46a19
iasminfoxgreen <iasmin@foxgreen.com.br>
2024-06-18 19:25:02 -0300

    config banco

--- backend/src/database/conexao.js
+++ backend/src/database/conexao.js
@@ -1,18 +1,27 @@
 require('dotenv').config();
 
-const { DB_HOST, DB_PORT,DB_USER, DB_NAME, DB_PASS } = process.env;
+// const { DB_HOST, DB_PORT,DB_USER, DB_NAME, DB_PASS } = process.env;
 
+
+// const knex = require("knex")({
+//   client: "pg",
+//   connection: {
+//     host: DB_HOST,
+//     port: DB_PORT,
+//     user: DB_USER,
+//     password: DB_PASS,
+//     database: DB_NAME,
+//     //ssl: { rejectUnauthorized: false }
+//   },
+// });
+
+// module.exports = knex;
+
+let { DB_URL } = process.env;
 
 const knex = require("knex")({
   client: "pg",
-  connection: {
-    host: DB_HOST,
-    port: DB_PORT,
-    user: DB_USER,
-    password: DB_PASS,
-    database: DB_NAME,
-    //ssl: { rejectUnauthorized: false }
-  },
+  connection: DB_URL
 });
 
 module.exports = knex;
